8704bd9c55615b97f9a5100c0c68cf1cace5a2bd
azishio <azishio3141@gmail.com>
2024-10-14 01:14:27 +0900

    🚚 ServerImpl内のステートの初期化処理をコンストラクタへ移動

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,9 +1,8 @@
 //! パスごとに呼び出される処理を定義
 
-use neo4rs::Graph;
-use reqwest::Client;
-
 use crate::cache::multi_layer::MultiLayerCache;
+use crate::env::EnvVars;
+use neo4rs::{ConfigBuilder, Graph};
 
 mod tile;
 mod sensor;
@@ -15,11 +14,38 @@ pub struct ServerImpl {
     graph: Graph,
     cache: MultiLayerCache,
     http_client: Client,
+    http_client: reqwest::Client,
 }
 impl ServerImpl {
     /// 新しいServerImplを作成する
     pub fn with_graph_and_cache(graph: Graph, cache: MultiLayerCache) -> Self {
         let http_client = Client::new();
+    pub async fn new() -> Self {
+        let env = EnvVars::read_env().unwrap();
+
+        let graph = {
+            let mut auth = env.neo4j_auth.split('/');
+            let neo4j_user = auth.next().unwrap();
+            let neo4j_password = auth.next().unwrap();
+
+            let config = ConfigBuilder::default()
+                .uri(env.neo4j_uri)
+                .user(neo4j_user)
+                .password(neo4j_password)
+                .db(env.neo4j_db)
+                .build()
+                .unwrap();
+            Graph::connect(config).await.unwrap()
+        };
+
+        let cache = MultiLayerCache::new(
+            env.memory_cache_max_size,
+            env.disk_cache_max_size,
+            env.disk_cache_base_path.into(),
+        ).await;
+
+
+        let http_client = reqwest::Client::new();
         Self {
             graph,
             cache,
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use axum::http::Method;
 use axum::serve;
-use neo4rs::{ConfigBuilder, Graph};
 use openapi::server::new;
 use tokio::net::TcpListener;
 use tower_http::cors::{AllowOrigin, CorsLayer};
 
 use crate::apis::ServerImpl;
-use crate::env::EnvVars;
-use cache::multi_layer::MultiLayerCache;
 
 mod apis;
 mod cache;
@@ -18,30 +15,7 @@ struct ServerState {}
 
 #[tokio::main]
 async fn main() {
-    let env = EnvVars::read_env().unwrap();
-
-    let graph = {
-        let mut auth = env.neo4j_auth.split('/');
-        let neo4j_user = auth.next().unwrap();
-        let neo4j_password = auth.next().unwrap();
-
-        let config = ConfigBuilder::default()
-            .uri(env.neo4j_uri)
-            .user(neo4j_user)
-            .password(neo4j_password)
-            .db(env.neo4j_db)
-            .build()
-            .unwrap();
-        Graph::connect(config).await.unwrap()
-    };
-
-    let cache = MultiLayerCache::new(
-        env.memory_cache_max_size,
-        env.disk_cache_max_size,
-        env.disk_cache_base_path.into(),
-    ).await;
-
-    let router = new(ServerImpl::with_graph_and_cache(graph, cache)).layer(
+    let router = new(ServerImpl::new()).layer(
         CorsLayer::new()
             .allow_origin(AllowOrigin::exact("http://localhost:3001".parse().unwrap()))
             .allow_methods(vec![Method::GET]),
