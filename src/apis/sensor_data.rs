81d4821a13e6e0a6a3ad7b8fe4c8fdeea0cefd85
azishio <azishio3141@gmail.com>
2024-10-14 00:33:17 +0900

    ✨ センサーデータの登録

--- src/apis/sensor_data.rs
+++ src/apis/sensor_data.rs
@@ -2,6 +2,8 @@ use crate::apis::ServerImpl;
 use axum::extract::Host;
 use axum::http::Method;
 use axum_extra::extract::CookieJar;
+use chrono::Local;
+use neo4rs::query;
 use openapi::apis::sensor_data::{PostApiSensorsDataResponse, SensorData};
 use openapi::models::{PostApiSensorsDataQueryParams, PostApiSensorsDataRequest};
 
@@ -13,6 +15,68 @@ impl SensorData for ServerImpl {
         query_params: PostApiSensorsDataQueryParams,
         body: Option<PostApiSensorsDataRequest>,
     ) -> Result<PostApiSensorsDataResponse, String> {
-        todo!()
+        let body = body.unwrap();
+        let PostApiSensorsDataRequest {
+            distance,
+            battery_voltage,
+            previous_sleep_time,
+            network_status,
+        } = body;
+
+        let query = query(
+            r#"
+// 1. センサーの取得
+MATCH (sensor:Sensor {id: $id})
+
+// 2. オプショナルマッチで現在のデータを取得
+OPTIONAL MATCH (sensor)-[:CURRENT_DATA]->(current_data:SensorData)
+
+// 3. 新しいセンサーデータの作成
+CREATE (new_data:SensorData {
+    distance: $distance,
+    battery_voltage: $battery_voltage,
+    previous_sleep_time: $previous_sleep_time,
+    network_status: $network_status,
+    time: $time
+})
+
+// 4. 条件分岐: current_data が存在するかどうか
+CALL apoc.do.when(
+    current_data IS NOT NULL,
+    // IF 部分: current_data が存在する場合の処理
+    '
+    // CURRENT_DATA リレーションシップを削除
+    DELETE (sensor)-[:CURRENT_DATA]->(current_data)
+    // 新しい CURRENT_DATA リレーションシップを作成
+    CREATE (sensor)-[:CURRENT_DATA]->(new_data)
+    // PREVIOUS_DATA リレーションシップを作成
+    CREATE (new_data)-[:PREVIOUS_DATA]->(current_data)
+    ',
+    // ELSE 部分: current_data が存在しない場合の処理
+    '
+    // 新しい CURRENT_DATA リレーションシップを作成
+    CREATE (sensor)-[:CURRENT_DATA]->(new_data)
+    ',
+    // パラメータ
+    {sensor: sensor, current_data: current_data, new_data: new_data}
+) YIELD value
+
+// 5. 新しいデータを返す
+RETURN sensor.interval AS interval
+            "#,
+        )
+            .param("id", query_params.id.to_string())
+            .param("distance", distance)
+            .param("battery_voltage", battery_voltage)
+            .param("previous_sleep_time", previous_sleep_time)
+            .param("network_status", network_status)
+            .param("time", Local::now().to_rfc3339());
+
+        let interval = self.graph.execute(query)
+            .await.unwrap().next()
+            .await.unwrap().unwrap()
+            .to::<i32>().unwrap();
+
+        Ok(PostApiSensorsDataResponse::Status200_OK(interval))
     }
 }
