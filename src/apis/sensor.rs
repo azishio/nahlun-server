6fff2cbe4b1622f662d5b7260a2e84a0475f9d13
azishio <azishio3141@gmail.com>
2024-10-16 23:18:24 +0900

    ✨ [post] /api/sensorsの実装

--- src/apis/sensor.rs
+++ src/apis/sensor.rs
@@ -39,6 +39,54 @@ RETURN id
         query_params: ApiSensorsPostQueryParams,
         body: Option<ApiSensorsPostRequest>,
     ) -> Result<ApiSensorsPostResponse, String> {
-        todo!()
+        let body = body.unwrap();
+        let ApiSensorsPostRequest { altitude, interval, scope, parent_node } = body;
+        let ApiSensorsPostQueryParams { id } = query_params;
+
+        let mut tnx = self.graph.start_txn().await.unwrap();
+        let _ = tnx
+            .run_queries([
+                query(r#"
+MERGE (sensor:Sensor{id:$id})
+ON CREATE
+ SET sensor.altitude=$altitude,
+     sensor.scope=$scope,
+     sensor.interval=$interval,
+     sensor.parent_node=$parent_node
+ON MATCH
+ SET sensor.altitude=$altitude,
+     sensor.scope=$scope,
+     sensor.interval=$interval,
+     sensor.parent_node=$parent_node
+
+WITH sensor
+MATCH (sensor)-[r:BELONGS_TO|AFFECTS]-(parent:RiverNode)
+DELETE r
+                "#),
+                query(r#"
+MATCH (parent:RiverNode{hilbert18:$parent_node})
+MATCH (sensor:Sensor{id:$id})
+CREATE (sensor)-[:BELONGS_TO]->(parent)
+
+WITH sensor, parent
+CALL apoc.path.expandConfig(parent, {
+    relationshipFilter: 'RIVER_LINK',
+    minLevel: 1,
+    maxLevel: 1000,
+    bfs: true,
+    limit: 1000
+}) YIELD path
+
+WITH sensor, parent, last(nodes(path)) AS target,
+     reduce(totalDistance = 0, rel in relationships(path) | totalDistance + rel.length) AS distance
+
+WHERE distance <= $scope
+CREATE (sensor)-[:AFFECTS {distance: distance}]->(target)
+                "#)
+            ]).await;
+
+        tnx.commit().await.unwrap();
+
+        Ok(ApiSensorsPostResponse::Status200)
     }
 }
